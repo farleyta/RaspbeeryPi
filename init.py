cd347548b05e5b8ac60b5f7a271b67ceec2f22c1
Tim Farley <me@timfarley.com>
2017-10-02 06:11:31 +0000

    Log amount poured afer 10s of inactivity.

--- init.py
+++ init.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python
+import time
 import RPi.GPIO as GPIO
+
+from flowmeter import *
 
 LEFT_TAP_GPIO = 18
 RIGHT_TAP_GPIO = 14
@@ -9,23 +13,37 @@ GPIO.setup(RIGHT_TAP_GPIO,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 print "start"
 
-leftPulses = 0
-rightPulses = 0
+left = FlowMeter('metric', ["BEER"])
+right = FlowMeter('metric', ["WATER"])
+minimumPour = 0.1
 
-def doAClick(channel):
-    global leftPulses
-    leftPulses += 1
-    print "Left pulse: ", leftPulses
-    # print "channel: " + channel
+def countLeftPulses(channel):
+    currentTime = int(time.time() * FlowMeter.MS_IN_A_SECOND)
+    if left.enabled == True:
+        left.update(currentTime)
 
-def doAClick2(channel):
-    global rightPulses
-    rightPulses += 1
-    print "Right pulse: ", rightPulses
+def countRightPulses(channel):
+    currentTime = int(time.time() * FlowMeter.MS_IN_A_SECOND)
+    if right.enabled == True:
+        right.update(currentTime)
 
-GPIO.add_event_detect(LEFT_TAP_GPIO, GPIO.RISING, callback=doAClick, bouncetime=20) # Left
-GPIO.add_event_detect(RIGHT_TAP_GPIO, GPIO.RISING, callback=doAClick2, bouncetime=20) # Right
+GPIO.add_event_detect(LEFT_TAP_GPIO, GPIO.RISING, callback=countLeftPulses, bouncetime=20)
+GPIO.add_event_detect(RIGHT_TAP_GPIO, GPIO.RISING, callback=countRightPulses, bouncetime=20)
 
 while True:
-    if leftPulses == 10 or rightPulses == 10:
-        break
+    currentTime = int(time.time() * FlowMeter.MS_IN_A_SECOND)
+    
+    if (left.thisPour > minimumPour and currentTime - left.lastClick > 10000): # 10 seconds of inactivity causes a tweet
+        print "Someone just poured " + left.getFormattedThisPour() + " of " + left.getBeverage() 
+        left.thisPour = 0.0
+
+    if (right.thisPour > minimumPour and currentTime - right.lastClick > 10000): # 10 seconds of inactivity causes a tweet
+        print "Someone just poured " + right.getFormattedThisPour() + " of " + right.getBeverage()
+        right.thisPour = 0.0
+
+    # reset flow meter after each pour (2 secs of inactivity)
+    if (left.thisPour <= minimumPour and currentTime - left.lastClick > 2000):
+        left.thisPour = 0.0
+    
+    if (right.thisPour <= minimumPour and currentTime - right.lastClick > 2000):
+        right.thisPour = 0.0
